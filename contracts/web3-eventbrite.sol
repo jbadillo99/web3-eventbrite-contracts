3ae4e1f3734eaa99a1cf29afd6330aeb06de74d9
Jesus Badillo <jbadillo2199@gmail.com>
2022-08-05 14:29:14 -0400

    added eth functionality to contract

--- contracts/web3-eventbrite.sol
+++ contracts/web3-eventbrite.sol
@@ -68,6 +68,55 @@ contract Web3EventBrite {
         // Require that the user sends enough ETH to cover the deposit
         require(msg.value == myEvent.deposit, "INSUFFICIENT FUNDS");
 
+        // Require that event has not already happened
+        require(block.timestamp <= myEvent.eventTimestamp, "EVENT HAS ALREADY PASSED");
+
+        // Require that the event is not at maximum capicity
+        require(myEvent.confirmedRSVPs.length < myEvent.maxCapacity, "EVENT HAS REACHED MAXIMUM CAPACITY");
+
+        // Require that the RSVP person has not already RSVP'd
+        for(uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(msg.sender != myEvent.confirmedRSVPs[i], "RSVP HAS ALREADY BEEN CONFIRMED");
+        }
+        
+        myEvent.confirmedRSVPs.push(payable(msg.sender));
+    }
+
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+
+        // Use CreateEvent struct based on eventId
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        // Require that the owner of the event is msg.sender
+        require(msg.sender == myEvent.eventOwner,"NOT AUTHORIZED");
+
+        // Require that the person who always checked in RSVP'd
+        address rsvpConfirmation;
+
+        for(uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            if( myEvent.confirmedRSVPs[i] == attendee ){
+                rsvpConfirmation = myEvent.confirmedRSVPs[i];
+            }
+        }
+
+        require(rsvpConfirmation == attendee, "ATTENDEE NOT IN RSVP LIST");
+
+        // Require that attendee has not already checked in
+        for(uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(attendee != myEvent.claimedRSVPs[i], "ATTENDEE HAS ALREADY CONFIRMED");
+        }
+
+        // Require that the attendee has not paid the owner yet
+        require(myEvent.paidOut == false, "ATTENDEE HAS ALREADY PAID");
+
+        // Send Eth back to the staker
+        (bool sent,) = attendee.call{value: myEvent.deposit}("");
+
+        if(sent == false){
+            myEvent.claimedRSVPs.pop();
+        }
+
+        require(sent, "FAILED TO SEND ETHER");
     }
 
 }
